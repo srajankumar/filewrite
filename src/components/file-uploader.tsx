ea400c1c354b2efabe5a94beaa883ee38f9cef23
Srajan Kumar <srajan.kumar@cognizant.com>
2025-10-07 09:47:02 +0530

    add: qr code (#6)

--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -13,7 +13,6 @@ import { useFileUpload } from "@/hooks/use-file-upload";
 import { Button } from "@/components/ui/button";
 import { supabase } from "@/lib/supabaseClient";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 import {
   Tooltip,
@@ -23,6 +22,8 @@ import {
 } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import Qr from "@/components/qr";
+import { Spinner } from "@/components/ui/spinner";
 
 function generateShortCode(length = 4) {
   const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -179,20 +180,7 @@ export default function FileUploader() {
           >
             {uploading ? "Uploading" : "Upload"}
             {uploading && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-4 w-4 animate-spin"
-              >
-                <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-              </svg>
+              <Spinner/>
             )}
           </Button>
         </div>
@@ -200,8 +188,8 @@ export default function FileUploader() {
 
       {/* success UI */}
       {downloadPageURL && (
-        <div className="mt-6 space-y-2">
-          <Label>Download link</Label>
+        <div className="mt-6 space-y-6">
+          <Qr link={downloadPageURL}/>
           <div className="flex gap-2">
             <div className="relative w-full">
               <Input
